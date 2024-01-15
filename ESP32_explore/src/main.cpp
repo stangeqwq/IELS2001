ec0d549825e59468f090bbb877498825a0a1facb
stangeqwq <StangelandEricJoshua@gmail.com>
2024-01-15 11:21:41 +0100

    m

--- ESP32_explore/src/main.cpp
+++ ESP32_explore/src/main.cpp
@@ -5,14 +5,11 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int read_p_1 = analogRead(39);
+  int read_p_2 = analogRead(34);
 }
